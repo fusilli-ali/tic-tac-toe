a0039a015ba7cbb10206a096394e8ab3e0c08afc
Alexandra B <abaldari@smarsh.com>
2016-11-30 15:27:15 -0800

    adding nov 30

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
+var winning = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+var xplays = [];
+var oplays = [];
+var oplay = 0;
+
 $(document).ready( function() {
-	$('div.grid').dblclick(function() {
-		$(this).html('<span class="x">X</span>');
-	})
+
+//write computerPlay function >> new random number every time i click....
+	function computerPlay() {
+		oplay = Math.floor((Math.random() * 9) +1);
+		return oplay;
+	}
+
+//computer adds random "O" to board
+	function RandomPlay() {
+		do {
+			computerPlay()
+		}
+		while  (xplays.includes(oplay) || (oplays.includes(oplay)));
+		var oplayString = oplay.toString();
+		$("[data-cell="+oplayString+"]").html('<span class="o">O</span>');
+	}
+
 	$('div.grid').click(function() {
-		$(this).html('<span class="o">O</span>');
+		$(this).html('<span class="x">X</span>');
+		xplays.push($(this).data('cell'));
+		RandomPlay();
 	})
 });
